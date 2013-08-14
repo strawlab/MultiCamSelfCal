ba46c27e147b3ae4c21ab36c5ea7f79a17460a4b
Andrew Straw <strawman@astraw.com>
2013-08-14 06:39:03 +0200

    python tests: add more data

--- python/test/test_mcsc.py
+++ python/test/test_mcsc.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil
 import multicamselfcal.execute
 from multicamselfcal.execute import MultiCamSelfCal
 
@@ -17,8 +17,15 @@ SRC_PATH = _get_src_path()
 DATA_PATH = _get_data_path()
 
 def test_mcsc():
-    calib_dir = os.path.join(DATA_PATH, 'caldata20130726_122220' )
+    paths = ['caldata20130726_122220', 'DATA20100906_134124' ]
+    for path in paths:
+        yield check_mcsc, path
+
+def check_mcsc(path):
+    calib_dir = os.path.join(DATA_PATH, path )
     mcscdir = os.path.join(SRC_PATH,'MultiCamSelfCal')
     mcsc = MultiCamSelfCal(calib_dir, mcscdir=mcscdir )
+    target_dir = os.path.join(calib_dir,'result')
+    shutil.rmtree(target_dir)
     caldir = mcsc.execute(silent=True)
     assert os.path.exists(caldir)
