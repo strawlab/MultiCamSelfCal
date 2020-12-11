92eb675278ffa5d553939ae2600a5c8fff1e934e
Andrew Straw <strawman@astraw.com>
2020-12-11 22:52:19 +0100

    metadata for pypi

--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
 setup(name='multicamselfcal',
+      description="wrapper for the Octave/Matlab multicamselfcal library",
       packages=['multicamselfcal'],
+      version='0.1.0',
+      author='John Stowers',
+      maintainer='Andrew Straw',
       )
