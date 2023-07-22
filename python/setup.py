554ad97f4f220f14b65cb2783c42063b10479e49
Andrew Straw <strawman@astraw.com>
2023-07-22 18:11:29 +0200

    convert python package to pyproject.toml

--- python/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='multicamselfcal',
-      description="wrapper for the Octave/Matlab multicamselfcal library",
-      packages=['multicamselfcal'],
-      version='0.1.0',
-      author='John Stowers',
-      maintainer='Andrew Straw',
-      )
